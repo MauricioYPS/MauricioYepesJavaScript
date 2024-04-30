c72f29c591c00394a4a3f6026ad7d16033dee2ff
MauricioYPS <yepes060@gmail.com>
2024-04-30 11:42:39 -0500

    Borre el respositorio clonado

---
+++ ejercicioBucles/JavaScript.js
@@ -0,0 +1,8 @@
+// 1. Realizar un programa que permita el ingreso de un numero y muestre su tabla de
+// multiplicar (Los primeros 10 multiplos).
+
+let num1,num2,resultado
+num1 = prompt("Ingrese un numero")
+for ( i =0 ; i<10 ; i++){
+    resultado = num1
+}
